17833e8ab758367ad23a4598d77319a807be5a72
Parth Darji <93513736+Parthdarji25@users.noreply.github.com>
2025-07-29 01:15:40 +0530

    db changes

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,11 @@ console.log("Using JWT secret:", SECRET_KEY);
 
 // MySQL connection (update with your credentials)
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "levelup_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 4000
 });
 
 // Middleware to authenticate JWT token for protected routes
